fd96887b9563dc3c5a0d0f0a4a46ee84692d6b31
Daniel Min <32945331+ddmin@users.noreply.github.com>
2019-02-07 17:43:10 -0500

    Add files via upload

---
+++ cmd.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('bdog.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('CMD').sheet1
+
+with open('command.txt') as f:
+    code = f.read().split('\n')
+
+for i, command in enumerate(code, 3):
+    sheet.update_cell(i, 1, command)
+
+sheet.update_cell(2, 1, 'RUN')
