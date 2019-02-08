343013e8fe88b1d6b5ad9df5bc81d9fc6cd3479d
Daniel Min <32945331+ddmin@users.noreply.github.com>
2019-02-07 23:08:45 -0500

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -7,21 +7,12 @@ client = gspread.authorize(creds)
 
 sheet = client.open('CMD').sheet1
 
-mode = input("W/D? >")
-if mode.lower() == 'd':
-    start = 2
-    while True:
-        if sheet.cell(start, 1).value != '':
-            sheet.update_cell(start, 1, '')
-            start += 1
-        else:
-            quit()
+file_name = input("File Name: >")
 
-else:
-    with open('command.txt') as f:
-        code = f.read().split('\n')
+with open('command.txt') as f:
+    code = f.read().split('\n')
 
-    for i, command in enumerate(code, 3):
-        sheet.update_cell(i, 1, command)
+for i, command in enumerate(code, 3):
+    sheet.update_cell(i, 1, command)
 
-    sheet.update_cell(2, 1, 'RUN')
+sheet.update_cell(2, 1, 'RUN')
